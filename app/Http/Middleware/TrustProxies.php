d9154dff527cf2d59c2cfcdc1173120d8f070ab8
grzesw <contact@kickasssubtitles.com>
2020-07-10 20:32:49 +0000

    Apply fixes from StyleCI

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,14 +2,21 @@
 
 declare(strict_types=1);
 
+/*
+ * KickAssSubtitles source code file
+ *
+ * @link      https://kickasssubtitles.com
+ * @copyright Copyright (c) 2016-2020
+ * @author    grzesw <contact@kickasssubtitles.com>
+ */
+
 namespace App\Http\Middleware;
 
 use Fideloper\Proxy\TrustProxies as Middleware;
 use Illuminate\Http\Request;
 
 /**
- * Class TrustProxies
- * @package App\Http\Middleware
+ * Class TrustProxies.
  */
 class TrustProxies extends Middleware
 {
